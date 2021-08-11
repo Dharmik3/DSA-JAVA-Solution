f839971f440b9367fc19428ce38360399c50adfc
Dharmik Patel <dharmikpatel.pepalla@gmail.com>
2021-08-11 11:34:10 +0530

    greeting name program

---
+++ assignments_solution/first java program/Name.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Name {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String name=in.nextLine();
+        System.out.println("Hello " +name+ ", \n\tIt's great to see you!\nThank you");
+    }
+}
